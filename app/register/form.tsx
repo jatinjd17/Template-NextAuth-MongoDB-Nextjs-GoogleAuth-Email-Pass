e00c81adb5f5b734f8b898882317d2cbbae6f865
jatinjd17 <69747287+jatinjd17@users.noreply.github.com>
2023-12-19 12:59:34 +0530

    second commit

--- app/register/form.tsx
+++ app/register/form.tsx
@@ -1,19 +1,41 @@
 "use client";
 
+import axios from "axios";
 import { FormEvent } from "react";
 
 export default function Form() {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
-    const response = await fetch(`http://localhost:5000/api/createaccount`, {
-      method: "POST",
-      body: JSON.stringify({
+    const response: any = await axios.post(
+      "http://localhost:5000/api/createaccount",
+      JSON.stringify({
         email: formData.get("email"),
         password: formData.get("password"),
       }),
-    });
-    console.log({ response });
+      {
+        headers: {
+          "Content-Type": "application/json",
+          // You can add additional headers here if needed
+        },
+      }
+    );
+
+    const { data } = response;
+
+    if (data.success) {
+      console.log({ success: data.success });
+    } else {
+      console.log({ Error: data.error });
+    }
+    // const response = await fetch(`http://localhost:5000/api/createaccount`, {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     email: formData.get("email"),
+    //     password: formData.get("password"),
+    //   }),
+    // });
+    // console.log({ response });
   };
   return (
     <form
